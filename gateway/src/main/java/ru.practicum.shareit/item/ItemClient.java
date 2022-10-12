2abc1aa42efe184f1d6f60f3c4e6c228d9f08959
AnnaLamonova <lamonova.a.v@yandex.ru>
2022-10-12 20:45:08 +0300

    hw 16 fix after 3 review

--- gateway/src/main/java/ru.practicum.shareit/item/ItemClient.java
+++ gateway/src/main/java/ru.practicum.shareit/item/ItemClient.java
@@ -70,15 +70,15 @@ public class ItemClient extends BaseClient {
 
     private void checkItemValid(ItemDto item) {
         if (item.getName() == null || item.getName().isBlank()) {
-            log.info("Field \"item name\" doesn't filled");
+            log.info("Field \"name\" doesn't filled in item {}", item);
             throw new ValidationException("Incorrect item name");
         }
         if (item.getDescription() == null || item.getDescription().isBlank()) {
-            log.info("Field \"item description\" doesn't filled");
+            log.info("Field \"description\" doesn't filled in item {}", item);
             throw new ValidationException("Incorrect item description");
         }
         if (item.getIsAvailable() == null) {
-            log.info("Field \"available\" doesn't filled");
+            log.info("Field \"available\" doesn't filled in item {}", item);
             throw new ValidationException("Field \"isAvailable\" must be filled");
         }
     }
